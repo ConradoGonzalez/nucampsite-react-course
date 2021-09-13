97cb2b250065a077a6cd4b15e54c8f0d69be128b
ConradoGonzalez <86978884+ConradoGonzalez@users.noreply.github.com>
2021-09-13 18:48:25 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,4 +1,3 @@
-import { isCommentNode } from "parse5/lib/tree-adapters/default";
 import React, { Component } from 'react';
 import { Card, Button, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
@@ -34,8 +33,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -49,29 +48,29 @@ class CommentForm extends Component {
                         <LocalForm onSubmit={values => this.handleSubmit(values)}>
                             <div className="form-group">
                                 <Label htmlFor="rating">Rating</Label>
-                                <Control.select 
-                                className="form-control" 
-                                model=".rating" 
-                                id="rating" 
-                                name="rating">
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
+                                <Control.select
+                                    className="form-control"
+                                    model=".rating"
+                                    id="rating"
+                                    name="rating">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
                                 </Control.select>
                             </div>
                             <div className="form-group">
                                 <Label htmlFor="author">Your Name</Label>
-                                <Control.text 
-                                className="form-control" 
-                                model=".author" 
-                                id="author" 
-                                name="author" 
-                                placeholder="Your Name"
-                                validators={{ 
-                                    minLength: minLength(2),
-                                    maxLength: maxLength(15)
+                                <Control.text
+                                    className="form-control"
+                                    model=".author"
+                                    id="author"
+                                    name="author"
+                                    placeholder="Your Name"
+                                    validators={{
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
                                     }}>
                                 </Control.text>
                                 <Errors
@@ -88,12 +87,12 @@ class CommentForm extends Component {
                             </div>
                             <div className="form-group">
                                 <Label htmlFor="text">Comment</Label>
-                                <Control.textarea 
-                                className="form-control" 
-                                model=".text" 
-                                id="text" 
-                                name="text" 
-                                rows="6">
+                                <Control.textarea
+                                    className="form-control"
+                                    model=".text"
+                                    id="text"
+                                    name="text"
+                                    rows="6">
                                 </Control.textarea>
                             </div>
                             <Button type="submit" value="submit" color="primary">Submit</Button>
@@ -118,7 +117,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -130,7 +129,7 @@ function RenderComments({ comments }) {
                             -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
                         </p>
                     </div>)}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>);
     } return <div />;
 }
@@ -151,7 +150,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
